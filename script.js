75e5e887c5ba08b4b4f7cfebcf43c7a7332fc309
Aleksei <103301@mail.ru>
2019-10-04 19:36:14 +0300

    модалки

--- script.js
+++ script.js
@@ -1,16 +1,64 @@
 let link = document.querySelector('.login');
 let popup = document.querySelector('footer + .substrate');
 let close = popup.querySelector('.close');
+let form = popup.querySelector("form");
+let login = popup.querySelector("[name=login]");
+let email = popup.querySelector("[name=email]");
+let isStorageSupport = true;
+let storage = "";
 
+try {
+    storage = localStorage.getItem("login");
+} catch (err) {
+    isStorageSupport = false;
+}
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-block");
+    if (storage) {
+        login.value = storage;
+        email.focus();
+    } else {
+        login.focus();
+    }
 });
 
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-block");
+    popup.classList.remove("modal-error");
+});
+
+popup.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-block");
+    popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!login.value || !email.value) {
+        evt.preventDefault();
+        popup.classList.remove("modal-error");
+        // noinspection SillyAssignmentJS
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-error");
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem("login", login.value);
+        }
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+    // noinspection JSDeprecatedSymbols
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains("modal-show")) {
+            popup.classList.remove("modal-show");
+            popup.classList.remove("modal-error");
+        }
+    }
 });
 
 //=============map-popup============================================
@@ -28,3 +76,10 @@ closeMap.addEventListener('click', function (par) {
     par.preventDefault();
     map.classList.remove('modal-map-block');
 });
+map.addEventListener('click', function (par) {
+    par.preventDefault();
+    map.classList.remove('modal-map-block');
+});
+// ========================================================================
+
+
