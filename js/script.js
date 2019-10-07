328e0a0d2e07845272fb55968588191ddda70044
Aleksei <103301@mail.ru>
2019-10-07 11:52:25 +0300

    закончено визуальное оформление, работа над кроссбраузерностью

--- js/script.js
+++ js/script.js
@@ -85,3 +85,30 @@ map.addEventListener('click', function (par) {
 // ========================================================================
 
 
+/*
+let imgObj = document.getElementById('image-2');
+function gray(imgObj) {
+    let canvas = document.createElement('canvas');
+    let canvasContext = canvas.getContext('2d');
+
+    let imgW = imgObj.width;
+    let imgH = imgObj.height;
+    canvas.width = imgW;
+    canvas.height = imgH;
+
+    canvasContext.drawImage(imgObj, 0, 0);
+    let imgPixels = canvasContext.getImageData(0, 0, imgW, imgH);
+
+    for(let y = 0; y < imgPixels.height; y++){
+        for(let x = 0; x < imgPixels.width; x++){
+            let i = (y * 4) * imgPixels.width + x * 4;
+            let avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+            imgPixels.data[i] = avg;
+            imgPixels.data[i + 1] = avg;
+            imgPixels.data[i + 2] = avg;
+        }
+    }
+    canvasContext.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+    return canvas.toDataURL();
+}
+imgObj.src = gray(imgObj);*/
